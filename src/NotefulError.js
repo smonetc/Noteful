2b0911a4af1485a950d599e5f549c2453e652baf
Monet Cooper <smomoc@monets-mbp.lan>
2020-09-04 20:13:08 -0400

    updated NotefulError

--- src/NotefulError.js
+++ src/NotefulError.js
@@ -12,11 +12,6 @@ class NotefulError extends React.Component{
         return { hasError: true };
       }
 
-    componentDidCatch(error, errorInfo) {
-   
-    logErrorToMyService(error, errorInfo);
-    }
-    
     render() {
         if (this.state.hasError) {      
             return (
